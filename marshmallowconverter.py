f8afc7760ef3cf000ea2b05f098a066674d45ae1
Jitse-Jan <jitse-jan.vanwatersc@sainsburys.co.uk>
2020-06-11 02:34:34 +0100

    Initial version of the Marshmallow converter

---
+++ marshmallowconverter.py
@@ -0,0 +1,123 @@
+""" marshmallowconverter.py """
+import json
+
+
+class MarshmallowConverter:
+    """ Defines the MarshmallowConverter class """
+
+    def __init__(self, inputdata):
+        self._input = self._set_input_data(inputdata)
+        self._schemas = []
+        self._schemas_output = ""
+        self._output = f"""from marshmallow import Schema, fields
+
+class ProspectSchema(Schema):
+"""
+        self._output += self.data_to_schema(self.input)
+
+    def write_output(self, outputfile):
+        with open(outputfile, "w") as fo:
+            fo.write(self.output)
+            fo.write(self.schemas_output)
+
+    def data_to_schema(self, inputdata, key="prospect"):
+        out = ""
+        if isinstance(inputdata, list):
+            print("inputdata is a list")
+            data = inputdata[0]
+        else:
+            data = inputdata
+        # Iterate
+        for key, value in data.items():
+            # print(key, value)
+            add = f"\t{key} = {self.get_type(key, value)}"
+            out += add + "\n"
+            print("> ", key, value, add)
+        return out
+
+    def get_type(self, key, value):
+        out = "fields."
+        if isinstance(value, str):
+            out += "Str("
+        elif isinstance(value, int):
+            out += "Int("
+        elif isinstance(value, bool):
+            out += "Bool("
+        elif isinstance(value, dict):
+            out += f"Nested(lambda: {self.get_schema_name(key)}()"
+            self.create_schema(key, value)
+        elif isinstance(value, list):
+            out += f"List("
+            if isinstance(value[0], dict):
+                out += f"fields.Nested({self.get_schema_name(key)})"
+                self.create_schema(key, value[0])
+        else:
+            out += "Str(allow_none=True"
+        out += ")"
+
+        return out
+        
+    def create_schema(self, key, value):
+        schema_name = self.get_schema_name(key)
+        print(">>", key, value, schema_name, schema_name in self.schemas)
+        if schema_name in self.schemas:
+            print(f"Skip {schema_name}")
+            return ""
+        else:
+            print(f">>> Creating schema for {schema_name}")
+            self.schemas.append(schema_name)
+            schema = f"\nclass {schema_name}(Schema):\n"
+            if isinstance(value, dict):
+                schema += self.data_to_schema(value)
+            self.schemas_output += schema
+
+    @staticmethod
+    def get_schema_name(key):
+        return f"{key.title().replace('_', '')}Schema"
+
+    @staticmethod
+    def _set_input_data(inputfile):
+        with open(inputfile, "r") as fh:
+            data = json.loads(fh.read())
+        if isinstance(data, list):
+            # If data is a list, create the schema for the first item
+            data = data[0]
+        return data
+
+    @property
+    def input(self):
+        return self._input
+
+    @property
+    def output(self):
+        return self._output
+
+    @output.setter
+    def output(self, value):
+        self._output += value
+
+    @property
+    def schemas(self):
+        return self._schemas
+
+    @schemas.setter
+    def schemas(self, value):
+        self._schemas = value
+
+    @property
+    def schemas_output(self):
+        return self._schemas_output
+
+    @schemas_output.setter
+    def schemas_output(self, value):
+        self._schemas_output = value
+
+
+conv = MarshmallowConverter("prospect.json")
+conv.write_output("ProspectOutputSchema.py")
+
+
+with open("prospect.json", "r") as fh:
+    data = json.loads(fh.read())
+from ProspectOutputSchema import ProspectSchema
+print(ProspectSchema(many=True).load(data))
